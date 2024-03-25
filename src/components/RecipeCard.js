56572ad6dce68c97e68463a0e29b4098f30e90b3
tnp94 <tnp94@yahoo.com>
2024-03-25 14:38:43 -0700

    Moved recipe cards to a separate component

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,24 +1,78 @@
-import { StyleSheet, View, Text, Image, FlatList } from "react-native";
+import { StyleSheet, View, Text, Image, FlatList, Alert } from "react-native";
 import { recipeList } from "../Recipes";
 import { createStackNavigator } from "@react-navigation/stack";
 import RecipeDetailsScreen from "../screens/RecipeDetailsScreen";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import storage from "../Storage";
 
-const Stack = createStackNavigator();
-const RecipeCard = () => {
+const RecipeCard = ({ item, index }) => {
+  const navigation = useNavigation()
+  
+  function recipeLongPress(index) {
+    Alert.alert('Recipe actions', 'My Alert Msg', [
+    {
+      text: 'Cancel',
+      onPress: () => console.log('Cancel Pressed'),
+      style: 'cancel',
+    },
+    {text: 'OK', onPress: () => console.log('OK Pressed')},
+    {text: 'Delete', onPress: () => deleteRecipe(index)},
+    ]);
+  }
+
+  function deleteRecipe(index) {
+    storage.load({key: "recipeList"}).then((recipeList) => {
+      recipeList.splice(index, 1)
+      storage.save({
+        key: "recipeList",
+        data: recipeList,
+        expires: null
+      })
+    })
+  }
+
   return (
-    <View>
-      <Text>Recipes</Text>
-        <FlatList 
-        data={recipeList} 
-        renderItem={({ item }) => (
-      <Stack.Navigator>
-          <Stack.Screen name="{item.name}" component={RecipeDetailsScreen} />
-            
-      </Stack.Navigator>
-        )}
-        keyExtractor={ item => item.id}
-        />
-    </View>
+    <TouchableOpacity 
+        onPress={() => navigation.navigate("Recipe Details",  { item } )}
+        onLongPress={() => {recipeLongPress(index)}}
+        key={index}
+        style={{
+          margin: 5,
+          padding: 5,
+          backgroundColor: '#fff',
+          borderColor: "#333",
+          borderRadius: 16,
+          alignItems: "center",
+          shadowOffset: {
+            width: 0,
+            height: 4
+          },
+          shadowColor: "#000",
+          shadowOpacity: 0.1,
+          flex: 1
+        }}
+        >
+          <Image
+          style={{
+            width: 70,
+            height: 70,
+            borderRadius: 20
+          }}
+          source={require("../../assets/images/ForkAndSpoon.png")} />
+          <Text style={{
+            flexWrap: "wrap",
+            flex: 1,
+            paddingHorizontal: 5,
+          }}>{index} {item.name}</Text>
+          <Text 
+          style={{
+            fontSize: 12,
+          }}>
+            Time: {(item.time.prep + item.time.active) !== 0 ? ( + (item.time.prep + item.time.active) + " minutes") : "?"}
+            {item.difficulty !== "" && (" | Difficulty: " + item.difficulty)}
+          </Text>
+        </TouchableOpacity>
   )
 }
 
--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Image, TouchableOpacity, SafeAreaView } from "react-native";
+import { StyleSheet, View, Text, Image, TouchableOpacity, SafeAreaView, Alert } from "react-native";
 // import { recipeList } from "../Recipes";
 import RecipeDetailsScreen from "../screens/RecipeDetailsScreen";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -6,6 +6,7 @@ import { FlatList } from "react-native-gesture-handler";
 import storage from "../Storage";
 import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import RecipeCard from "./RecipeCard";
 
 Stack = createStackNavigator()
 const RecipeCards = ( ) => {
@@ -32,52 +33,20 @@ const RecipeCards = ( ) => {
 
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{
+      flex: 1,
+    }}>
       <FlatList
-      // {recipeList.map(( item ) => (
         data={data}
         numColumns={2}
-        renderItem={ ({ item }) => (
-        <TouchableOpacity 
-        onPress={() => navigation.navigate("Recipe Details",  { item } )}
-        key={item.id}
-        style={{
-          margin: 5,
-          padding: 5,
-          backgroundColor: '#fff',
-          borderColor: "#333",
-          borderRadius: 16,
-          alignItems: "center",
-          shadowOffset: {
-            width: 0,
-            height: 4
-          },
-          shadowColor: "#000",
-          shadowOpacity: 0.1,
-          flex: 0.5
-        }}
-        >
-          <Image
-          style={{
-            width: 70,
-            height: 70,
-            borderRadius: 20
-          }}
-          source={require("../../assets/images/ForkAndSpoon.png")} />
-          <Text style={{
-            flexWrap: "wrap",
-            flex: 1,
-            paddingHorizontal: 5,
-          }}>{item.name}</Text>
-          <Text 
-          style={{
-            fontSize: 12,
+        renderItem={ ({ item, index }) => (
+          <View style={{
+            flex: 0.5,
+            alignContent: "center"
           }}>
-            Time: {(item.time.prep + item.time.active) !== 0 ? ( + (item.time.prep + item.time.active) + " minutes") : "?"}
-            {item.difficulty !== "" && (" | Difficulty: " + item.difficulty)}
-          </Text>
-        </TouchableOpacity>
-      )}
+            <RecipeCard item={item} index={index}/>
+          </View>
+        )}
       />
     </View>
   )
